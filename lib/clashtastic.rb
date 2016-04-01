a3b9b29769c5f0c75dd42d2bc99c8d9658673456
Purrbiscuit <spurr@articulate.com>
2016-04-01 12:42:22 -0400

    basic outline of api operations to query

--- lib/clashtastic.rb
+++ lib/clashtastic.rb
@@ -1,2 +1,42 @@
-require 'httparty'
-require 'pry'
+require "httparty"
+
+class Clashtastic
+  def initialize(jwt_token)
+    @jwt_token = jwt_token
+  end
+
+  def query(query)
+    HTTParty.get("https://api.clashofclans.com/v1/#{query}", :headers => {'Authorization' => 'Bearer ' + @jwt_token})
+  end
+
+  #Clan Things
+  def search_clans()
+
+  end
+
+  def clan_info()
+
+  end
+
+  def list_members()
+
+  end
+
+  #Location Things
+  def list_locations()
+
+  end
+
+  def location_info()
+
+  end
+
+  def location_rankings()
+
+  end
+
+  #League Things
+  def list_leagues()
+
+  end
+end
